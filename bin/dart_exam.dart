ebde700286fc8e477890a690a9b9e27502b4c3fa
SangWon7242 <gotkdqja@gmail.com>
2024-07-12 20:25:38 +0900

    null safety

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,26 +1,25 @@
-import 'package:args/args.dart';
-
 void main() {
-  var name; // Dynamic으로 변수를 선언(원하는 데이터 할당 가능)
-  name = '홍길동';
-  name = 10;
-  name = true;
-  // 이게 가능한 이유는 변수 타입이 dynamic 이기 때문에
-  // 그럼 왜 사용하는가?
-  // 변수가 어떤 타입일지 알기 어려운 경우가 있기 때문
-
-  dynamic name2; // 이렇게도 타입을 명시할 수 있음
   /*
-  if(name2 is String) {
-    name2. // 해당 블럭 안에서는 name이 String인걸 안다.
-  }
-   */
+  String name = '홍길동';
+  name = null; // 이런 경우 name 변수에는 string 타입만 넣을 수 있음
+  */
 
-  // 인터넷에서 어떤 데이터를 받았는데, 그 데이터가 어떤 타입인지 모른 경우
+  String? name = '홍길동'; // String? 붙이면 에러가 사라짐
+  name = null;
+  
+  // 컴파일러가 해당 변수가 null이 될 수도 있다는 걸 알기 때문
 
-  /*
-  if(name2 is int) {
-    name2. // 해당 블럭 안에서는 name이 int인걸로 인식
+  // name.length --> name이 null 이 될 수 있기에 실행x
+  // length 사용 전 조건문을 통한 확인이 필요
+
+  if(name != null) {
+    name.isNotEmpty; // 이 부분에서 해당 변수가 null이 아님을 확인
   }
-   */
+
+  // null safety 라는 건 변수 혹은 데이터가 null이 될 수 있음을 명시하는 걸 말함
+  // 모든 변수는 non-nullable이다. null 이 될 수 없음을 의미
+  // 단, null이 될 수 있다는 조건을 부여하려면 String? `?`를 붙여주면 됨
+
+  // 위 코드를 단축한 표현
+  name?.isNotEmpty; // name이 null이 아니면 해당 속성을 달라 요청
 }
