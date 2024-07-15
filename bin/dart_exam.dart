69eaa323f70b37cb3e93ca1bfb4909775c04ce21
SangWon7242 <gotkdqja@gmail.com>
2024-07-15 20:38:30 +0900

    dart 자료형

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,9 +1,8 @@
 void main() {
-  // const : compile-time constant를 만들어줌
-
-  const name = '홍길동';
-  // const라 하면 해당 값이 컴파일이 될 때 그 값을 알고 있음
-  // const API = 'API-KEY'
-  // 단 API에서 받아오는 값은 final로 선언
-  // 앱스토어에 올리기 전에 값을 알고 있어야 함
+  String name = '홍길동';
+  bool isMarried = true;
+  int age = 12; // num 을 상속받음
+  double pi = 3.14; // num 을 상속받음
+  num x = 12;
+  x = 3.14;
 }
