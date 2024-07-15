65f3deab5f31112a5851f9635fae0727b2ccfe92
SangWon7242 <gotkdqja@gmail.com>
2024-07-15 21:05:28 +0900

    Lists

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,8 +1,28 @@
 void main() {
-  String name = '홍길동';
-  bool isMarried = true;
-  int age = 12; // num 을 상속받음
-  double pi = 3.14; // num 을 상속받음
-  num x = 12;
-  x = 3.14;
+  // var numbers = [1, 2, 3, 4, 5];
+  List<int> numbers = [1, 2, 3, 4, 5];
+  // numbers.add('안녕'); // 타입불일치로 안됨
+  numbers.add(6);
+  print(numbers.first); // 리스트 첫번 째 요소 반환
+  print(numbers.last); // 리스트 마지막 요소 반환
+  print(numbers.isEmpty); // 비어있는지 확인
+  print(numbers.contains(3)); // 해당 값이 존재하는지 확인
+
+  /* collectio if */
+
+  var giveMeFive = true;
+  List<int> nums = [
+    1,
+    2,
+    3,
+    4,
+    // if(giveMeFive) 5
+  ];
+
+  // 같은 의미
+  if(giveMeFive) {
+    nums.add(5);
+  }
+
+  print(nums);
 }
