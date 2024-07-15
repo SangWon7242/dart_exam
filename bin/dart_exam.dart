36b4192c51ae0787424a55cab41147f6aa9279be
SangWon7242 <gotkdqja@gmail.com>
2024-07-15 21:21:00 +0900

    Collection For

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,16 +1,19 @@
 void main() {
+  // Collection For
+  var favoriteFruits = ['바나나', '사과', '복숭아'];
+  var newFruits = [
+    '용과',
+    '수박',
+    '포도',
+    for(var fruits in favoriteFruits) "🤩 $fruits"
+  ];
+
   /*
-  var name = '홍길동';
-  // 문자열 출력 방법 $
-  var introduce = '안녕하세요. 제 이름은 $name 입니다.';
+  // 같은 문법
+  for(var fruits in favoriteFruits) {
+    newFruits.add(fruits);
+  }
   */
 
-  var name = '홍길동';
-  var age = 20;
-  var introduce = '안녕하세요. 제 이름은 $name이고, 나이는 $age살 입니다.';
-  print(introduce);
-  
-  // ${age + 2} --> 무언가를 계산하고 싶을 때 사용
-  introduce = '안녕하세요. 제 이름은 \'$name\'이고, 나이는 ${age + 2}살 입니다.';
-  print(introduce);
+  print(newFruits);
 }
